d8648089b91ea77436d38c7ce011f4f3eb7544c1
austinmacmath <47233575+austinmacmath@users.noreply.github.com>
2019-09-10 17:57:15 -0700

    Update AgentsNear.sql

--- AgentsNear.sql
+++ AgentsNear.sql
@@ -3,4 +3,4 @@ RETURNS BOOLEAN AS
 $$ SELECT POWER(POWER((agent_lat - prop_lat), 2) + POWER((agent_long - prop_long), 2), 0.5) < radius $$ 
 LANGUAGE SQL;
 
-SELECT "name" FROM "ApplicantAgents" WHERE PointInCircle("latitude", "longitude", 70, 50, 999)
+SELECT "name" FROM "ApplicantAgents" WHERE PointInCircle("latitude", "longitude", property_latitude, property_longitude, radius)
