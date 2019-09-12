cdf5275a16563be1aae838a974fce8eece37eb02
austinmacmath <47233575+austinmacmath@users.noreply.github.com>
2019-09-12 10:11:32 -0700

    Update AgentsNear.sql

--- AgentsNear.sql
+++ AgentsNear.sql
@@ -1,6 +1,6 @@
 CREATE FUNCTION PointInCircle(agent_lat FLOAT, agent_long FLOAT, prop_lat FLOAT, prop_long FLOAT, radius FLOAT)
 RETURNS BOOLEAN AS 
-$$ SELECT POWER(POWER((agent_lat - prop_lat), 2) + POWER((agent_long - prop_long), 2), 0.5) < radius $$ 
+$$ SELECT POWER(POWER((agent_lat - prop_lat), 2) + POWER((agent_long - prop_long), 2), 0.5) <= radius $$ 
 LANGUAGE SQL;
 
-SELECT "name" FROM "ApplicantAgents" WHERE PointInCircle("latitude", "longitude", property_latitude, property_longitude, radius)
+SELECT "name", "streetNumber", "address", "city", "phoneNumber", "email" FROM "ApplicantAgents" WHERE PointInCircle("latitude", "longitude", 70, 50, 999)
